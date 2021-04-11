aabb9699571aac87b5dc2f9bc0defbdf9d8407a5
Saull Brandão <saull@outlook.com>
2021-04-11 08:45:15 -0300

    Make the elements to pass the tests

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
+import './Counter.css'
+
 const Counter = () => {
+    const [counterValue, setCounterValue] = useState(0)
+    const [inputValue, setInputValue] = useState(1)
+
     return (
-        <h2 data-testid="header">My Counter</h2>
+        <div>
+            <h2 data-testid='header'>My Counter</h2>
+            <h1 data-testid='counter'>{counterValue}</h1>
+            <button data-testid='subtract-btn'>-</button>
+            <input
+                data-testid='input'
+                type='number'
+                value={inputValue}
+                className='text-center'
+            />
+            <button data-testid='add-btn'>+</button>
+        </div>
     )
 }
 
