567baf825157bf20515c96437e5932bee2dab19c
Saull Brandão <saull@outlook.com>
2021-04-11 08:44:29 -0300

    Create tests for the elements of the component

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -10,3 +10,30 @@ test('header renders with correct text', () => {
     expect(headerEl.textContent).toBe("My Counter")
 })
 
+test('counter initially start with text of 0', () => {
+    const { getByTestId } = render(<Counter />)
+    const counterEl = getByTestId('counter')
+
+    expect(counterEl.textContent).toBe('0')
+})
+
+test('input contains initial value of 1', () => {
+    const { getByTestId } = render(<Counter />)
+    const inputEl = getByTestId('input')
+
+    expect(inputEl.value).toBe('1')
+})
+
+test('add button renders with +', () => {
+    const { getByTestId } = render(<Counter />)
+    const addButton = getByTestId('add-btn')
+
+    expect(addButton.textContent).toBe('+')
+})
+
+test('subtract button renders with -', () => {
+    const { getByTestId } = render(<Counter />)
+    const subtractButton = getByTestId('subtract-btn')
+
+    expect(subtractButton.textContent).toBe('-')
+})
